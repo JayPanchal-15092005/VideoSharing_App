424d3fbde2f3001d74e43ad55acf52f218967981
JayPanchal-15092005 <trywebsiteapp56@gmail.com>
2025-05-04 15:42:04 +0530

    Video Sharing App Commit

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,32 @@
+import { Stack, Redirect } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { Loader } from "../../components/index";
+import { useGlobalContext } from "../../context/GlobalProvider.js"
+
+const AuthLayout = () => {
+
+  const { loading, isLogged } = useGlobalContext();
+
+  if(!loading && isLogged) return <Redirect href={"/home"} />
+
+  return (
+    <>
+    <Stack>
+      <Stack.Screen
+      name='sign-in'
+      options={{ headerShown: false }}
+      />
+      <Stack.Screen
+      name='sign-up'
+      options={{ headerShown: false }}
+      />
+    </Stack>
+
+    <Loader isLoading={loading}/>
+
+    <StatusBar backgroundColor='#161622' style='light' />
+    </>
+  )
+}
+
+export default AuthLayout;
---
+++ components/index.js
@@ -0,0 +1,19 @@
+import CustomButton from "./CustomButton.jsx";
+import Loader from "./Loader.jsx";
+import FormField from "./FormField.jsx";
+import InfoBox from "./InfoBox.jsx";
+import EmptyState from "./EmptyState.jsx";
+import SearchInput from "./SearchInput.jsx";
+import Trending from "./Trending.jsx";
+import VideoCard from "./VideoCard.jsx";
+
+export {
+    CustomButton,
+    Loader,
+    FormField,
+    InfoBox,
+    EmptyState,
+    SearchInput,
+    Trending,
+    VideoCard,
+}
